4d09cdf6b8a735618e70020167c0fa275576e54a
anenae <84938376+anenae@users.noreply.github.com>
2021-07-26 22:13:13 +0200

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "holberton.h"
 #include <stdlib.h>
 
@@ -6,7 +7,37 @@
  *
  *Return: always 1
  */
+int buffer_print(char buffer[], unsigned int nb)
+{
+	write(1, buffer, nb);
+	return (nb);
+}
+/**
+ * buffer_add - adds a string to buffer
+ * Return: if buffer filled and emptyed return number of printed char
+ * else 0
+ */
+int buffer_add(char buffer[], char *str, unsigned int *buffer_pos)
+{
+	int i = 0;
+	unsigned int count = 0, pos = *buffer_pos, size = BUFFER_SIZE;
 
+	while (str && str[i])
+	{
+		if (pos == size)
+		{
+			count += buffer_print(buffer, pos);
+			pos = 0;
+		}
+		buffer[pos++] = str[i++];
+	}
+	*buffer_pos = pos;
+	return (count);
+}
+/**
+ * _printf - produces output according to a format
+ * @format: character string
+ */
 int _printf(const char *format, ...)
 {
 	char ch = (char)va_arg(c, int);
