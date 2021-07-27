3f88387177b2e9ec301bca26dd2c61b91e584853
anenae <84938376+anenae@users.noreply.github.com>
2021-07-27 07:30:35 +0200

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,19 +1,26 @@
 #include <unistd.h>
 #include "holberton.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- *_printf: prints a char
+ * buffer_print - print given buffer to stdout
+ * @buffer: buffer to print
+ * @nbytes: number of bytes to print
  *
- *Return: always 1
+ * Return: nbytes
  */
-int buffer_print(char buffer[], unsigned int nb)
+int buffer_print(char buffer[], unsigned int nbytes)
 {
-	write(1, buffer, nb);
-	return (nb);
+	write(1, buffer, nbytes);
+	return (nbytes);
 }
+
 /**
  * buffer_add - adds a string to buffer
+ * @buffer: buffer to fill
+ * @str: str to add
+ * @buffer_pos: pointer to buffer first empty position
+ *
  * Return: if buffer filled and emptyed return number of printed char
  * else 0
  */
@@ -34,33 +41,43 @@ int buffer_add(char buffer[], char *str, unsigned int *buffer_pos)
 	*buffer_pos = pos;
 	return (count);
 }
+
 /**
  * _printf - produces output according to a format
  * @format: character string
- */
-int _printf(const char *format, ...)
-{
-	char ch = (char)va_arg(c, int);
-
-	_putchar(ch);
-	return(1);
-}
-
-/**
- *_printf: prints a string
  *
- *Return: The number of chars printed
+ * Return: the number of characters printed excluding the null byte
+ * used to end output to strings
  */
 int _printf(const char *format, ...)
 {
-	int count;
-	char *str = va_arg(s. char *);
+	va_list ap;
+	unsigned int i = 0, buffer_pos = 0, count = 0;
+	char *res_str, *aux, buffer[BUFFER_SIZE];
 
-	if (str == NULL)
-		str = "(null)";
-	for (count = 0; str[count]; count++)
+	if (!format || !format[0])
+		return (-1);
+	va_start(ap, format);
+	aux = malloc(sizeof(char) * 2);
+	while (format && format[i])
 	{
-		_putchar(str[count]);
+		if (format[i] == '%')
+		{
+			res_str = treat_format(format, &i, ap);
+			count += buffer_add(buffer, res_str, &buffer_pos);
+			free(res_str);
+		}
+		else
+		{
+			aux[0] = format[i++];
+			aux[1] = '\0';
+			count += buffer_add(buffer, aux, &buffer_pos);
+		}
 	}
+	count += buffer_print(buffer, buffer_pos);
+	free(aux);
+	va_end(ap);
+	if (!count)
+		count = -1;
 	return (count);
 }
